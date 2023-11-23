665555a0c916542a4bc9140632723c490ec26828
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-11-23 22:49:20 +0300

    Implement interface repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,15 @@ import java.util.List;
 //@Component
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Hassan",
-                        "hassana@yahoo.com",
-                        LocalDate.of(1994, Month.FEBRUARY, 22),
-                        33
-                )
-        );
+        return studentRepository.findAll();
     }
 }

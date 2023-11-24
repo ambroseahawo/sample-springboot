6c1934c9c17add8f99d981af456ff1d7b04ec761
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-11-24 23:34:12 +0300

    Check if email already exists

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 //@Component
 @Service
@@ -22,5 +23,11 @@ public class StudentService {
 
     public void addNewStudent(Student student) {
         System.out.println((student));
+
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail((student.getEmail()));
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken!!");
+        }
+        studentRepository.save(student);
     }
 }
